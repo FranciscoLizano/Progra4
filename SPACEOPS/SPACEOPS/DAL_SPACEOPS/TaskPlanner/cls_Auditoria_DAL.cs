648560a543799ff5da32a7df7c1373bd64bb2f42
FranciscoLizano <frligu73@gmail.com>
2026-03-10 21:36:39 -0600

    Cambios 20260310

---
+++ SPACEOPS/SPACEOPS/DAL_SPACEOPS/TaskPlanner/cls_Auditoria_DAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_SPACEOPS.TaskPlanner
+{
+    public class cls_Auditoria_DAL
+    {
+        #region Variables Privadas
+
+        /*Sección de campos de la tabla*/
+        private int _iId_Auditoria, _iId_Usuario;
+        private string _sAccion, _sDescripción;
+        private DateTime _dfecha;
+        /*Sección de campos de la tabla*/
+
+        /*Sección presente en todas las clases*/
+        private string _sValorScalar, _sAXN, _sMSJError;
+        private DataTable _dtDatos, _dtParametros;
+        /*Sección presente en todas las clases*/
+        #endregion
+
+        #region Variables Públicas o Constructores
+        public int iId_Auditoria { get => _iId_Auditoria; set => _iId_Auditoria = value; }
+        public int iId_Usuario { get => _iId_Usuario; set => _iId_Usuario = value; }
+        public string sAccion { get => _sAccion; set => _sAccion = value; }
+        public string sDescripción { get => _sDescripción; set => _sDescripción = value; }
+        public DateTime dfecha { get => _dfecha; set => _dfecha = value; }
+        public string sValorScalar { get => _sValorScalar; set => _sValorScalar = value; }
+        public string sAXN { get => _sAXN; set => _sAXN = value; }
+        public string sMSJError { get => _sMSJError; set => _sMSJError = value; }
+        public DataTable stDatos { get => _dtDatos; set => _dtDatos = value; }
+        public DataTable stParametros { get => _dtParametros; set => _dtParametros = value; }
+        #endregion
+    }
+}
---
+++ SPACEOPS/SPACEOPS/DAL_SPACEOPS/TaskPlanner/cls_Tareas_DAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_SPACEOPS.TaskPlanner
+{
+    public class cls_Tareas_DAL
+    {
+        #region Variables Privadas
+        /*Sección de campos de la tabla*/
+        private int _iId_Tarea, _iId_Usuario, _iId_Estado, _iId_Prioridad, _iId_Actividad;
+        private string _sTitulo, _sDescripcion;
+        private DateTime _dFecha_Registro, _dFecha_Inicio, _dFecha_Finalizacion;
+        /*Sección de campos de la tabla*/
+
+        /*Sección presente en todas las clases*/
+        private string _sValorScalar, _sAXN, _sMSJError;
+        private DataTable _dtDatos, _dtParametros;
+        /*Sección presente en todas las clases*/
+        #endregion
+
+        #region Variables Públicas o Constructores
+        public int iId_Tarea { get => _iId_Tarea; set => _iId_Tarea = value; }
+        public int iId_Usuario { get => _iId_Usuario; set => _iId_Usuario = value; }
+        public int iId_Estado { get => _iId_Estado; set => _iId_Estado = value; }
+        public int iId_Prioridad { get => _iId_Prioridad; set => _iId_Prioridad = value; }
+        public int iId_Actividad { get => _iId_Actividad; set => _iId_Actividad = value; }
+        public string sTitulo { get => _sTitulo; set => _sTitulo = value; }
+        public string sDescripcion { get => _sDescripcion; set => _sDescripcion = value; }
+        public DateTime dFecha_Registro { get => _dFecha_Registro; set => _dFecha_Registro = value; }
+        public DateTime dFecha_Inicio { get => _dFecha_Inicio; set => _dFecha_Inicio = value; }
+        public DateTime dFecha_Finalizacion { get => _dFecha_Finalizacion; set => _dFecha_Finalizacion = value; }
+        public string sValorScalar { get => _sValorScalar; set => _sValorScalar = value; }
+        public string sAXN { get => _sAXN; set => _sAXN = value; }
+        public string sMSJError { get => _sMSJError; set => _sMSJError = value; }
+        public DataTable dtDatos { get => _dtDatos; set => _dtDatos = value; }
+        public DataTable dtParametros { get => _dtParametros; set => _dtParametros = value; }
+        #endregion
+
+    }
+}
